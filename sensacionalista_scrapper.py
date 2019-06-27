4a13a562377f55f56d50dcd904f22b0c948976f0
jairojuunior <jairodasilvafreitasjunior@hotmail.com>
2019-06-27 20:24:34 -0300

    Continuation of scrapper using scrappy

--- sensacionalista_scrapper.py
+++ sensacionalista_scrapper.py
@@ -1,5 +1,17 @@
 import scrapy
 
+#td-module-thumb
+#entry-title
+#start_urls = ['https://www.sensacionalista.com.br/pais/', 'https://www.sensacionalista.com.br/entretenimento/']
+#//div[contains(@class, "entry-title")]/a::text()
+
 class SensacionalistaScrapper(scrapy.Spider):
-    name = "sensacionalista_scrapper"
-    start_urls = ['https://www.sensacionalista.com.br/pais/', 'https://www.sensacionalista.com.br/entretenimento/']
+  name = "sensacionalista_scrapper"
+  start_urls = ['https://www.sensacionalista.com.br/pais/']
+
+  def parse(self, response):
+    SET_SELECTOR = '//div[contains(@class, "entry-title")]/a::text()'
+    print(response)
+    #for news in response.xpath(SET_SELECTOR):
+      #news_title = news.extract()
+      #print(news_title)
